38a22b6b34fe79e1eac64f40c03614850b23199f
indrajith3110 <indrajithsomaiah2310@gmail.com>
2025-04-03 14:25:42 +0530

    Initialize project with Docker setup, TypeScript configuration, and basic Hono.js server

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+ 
+const app = new Hono();
+
+app.get('/ping', (c) => {
+    return c.json({ pong: 'Hello, World' });
+});
+
+app.get('/college', (c) => {
+    return c.text('Hello, College! This is a test for Hono.js');
+});
+     
+
+export default app;
+
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
